481870db973f02da7b3fca428c61c4e89d45c51a
Harrison Deng <yunyangdeng@outlook.com>
2025-03-13 14:25:44 +0000

    Updated tests to reflect new fasta read naming

--- tests/autobigs/engine/test_reading.py
+++ tests/autobigs/engine/test_reading.py
@@ -4,4 +4,9 @@ from autobigs.engine.reading import read_fasta
 async def test_fasta_reader_not_none():
     named_strings = await read_fasta("tests/resources/tohama_I_bpertussis.fasta")
     for named_string in named_strings:
-        assert named_string.name == "BX470248.1"
+        assert named_string.name is not None
+
+async def test_fasta_reader_name_contains_file_and_id():
+    named_strings = await read_fasta("tests/resources/tohama_I_bpertussis.fasta")
+    for named_string in named_strings:
+        assert named_string.name == "tohama_I_bpertussis.fasta:BX470248.1"
